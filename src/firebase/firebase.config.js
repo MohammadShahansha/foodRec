29ac7a3bd86fb6b97968b5c29cbaeca4c39aaecb
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-05-03 13:42:08 +0600

    added login and register layout

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCc0wTtJ7g64iT-KAVH6SwZuVafLxpYlk",
+  authDomain: "for-assignment-ten.firebaseapp.com",
+  projectId: "for-assignment-ten",
+  storageBucket: "for-assignment-ten.appspot.com",
+  messagingSenderId: "599660008423",
+  appId: "1:599660008423:web:0a012a717a990bb6b9a342"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
